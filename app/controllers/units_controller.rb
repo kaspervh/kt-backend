1d23d5530c08c12044e01778ff05d5f946cf02fb
kaspervh <kaspervhauschildt@protonmail.com>
2022-11-10 12:39:53 +0100

    added test seeds

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -3,49 +3,54 @@ class UnitsController < ApplicationController
 
   # GET /units
   def index
-    @units = Unit.all
-
-    render json: @units
-  end
-
-  # GET /units/1
-  def show
-    render json: @unit
-  end
-
-  # POST /units
-  def create
-    @unit = Unit.new(unit_params)
-
-    if @unit.save
-      render json: @unit, status: :created, location: @unit
-    else
-      render json: @unit.errors, status: :unprocessable_entity
+    @units = Unit.where(faction_id: params[:faction_id])
+    units = @units.map do |unit|
+      p unit 
     end
+    
+    render json: units
   end
 
-  # PATCH/PUT /units/1
-  def update
-    if @unit.update(unit_params)
-      render json: @unit
-    else
-      render json: @unit.errors, status: :unprocessable_entity
-    end
-  end
+  # Out cmmented untill admin user has been set up
 
-  # DELETE /units/1
-  def destroy
-    @unit.destroy
-  end
+  # # GET /units/1
+  # def show
+  #   render json: @unit
+  # end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_unit
-      @unit = Unit.find(params[:id])
-    end
+  # # POST /units
+  # def create
+  #   @unit = Unit.new(unit_params)
 
-    # Only allow a list of trusted parameters through.
-    def unit_params
-      params.require(:unit).permit(:name, :weapon_skill, :balistics_skill, :strength, :toughness, :wounds, :initiative, :attacks, :leadership, :armor_save, :unit_type, :price)
-    end
+  #   if @unit.save
+  #     render json: @unit, status: :created, location: @unit
+  #   else
+  #     render json: @unit.errors, status: :unprocessable_entity
+  #   end
+  # end
+
+  # # PATCH/PUT /units/1
+  # def update
+  #   if @unit.update(unit_params)
+  #     render json: @unit
+  #   else
+  #     render json: @unit.errors, status: :unprocessable_entity
+  #   end
+  # end
+
+  # # DELETE /units/1
+  # def destroy
+  #   @unit.destroy
+  # end
+
+  # private
+  #   # Use callbacks to share common setup or constraints between actions.
+  #   def set_unit
+  #     @unit = Unit.find(params[:id])
+  #   end
+
+  #   # Only allow a list of trusted parameters through.
+  #   def unit_params
+  #     params.require(:unit).permit(:name, :weapon_skill, :balistics_skill, :strength, :toughness, :wounds, :initiative, :attacks, :leadership, :armor_save, :unit_type, :price)
+  #   end
 end
